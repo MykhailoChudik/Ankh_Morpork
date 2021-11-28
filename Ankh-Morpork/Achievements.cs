47e08e6758d9a9539f56731c159a80c965850089
MykhailoChudik <mykhailo.chudik@valtech.com>
2021-11-28 04:56:01 +0200

    Add achievements

---
+++ Ankh-Morpork/Achievements.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ankh_Morpork
+{
+    public interface IAchievements
+    {
+        void Update(IGame game, int type);
+    }
+
+    class Achievements : IAchievements
+    {
+        private bool _goldAchievement = false;
+        private int _countFool = 0;
+        private int _countBeggary = 0;
+        private int _countAssassin = 0;
+        private int _countThieves = 0;
+
+
+        public void Update(IGame game, int type)
+        {
+            switch (type)
+            {
+                case -1:
+                    GoldAchievement(game);
+                    break;
+                case 0:
+                    FoolsAchievement(game);
+                    break;
+                case 1:
+                    BeggaryAchievement(game);
+                    break;
+                case 2:
+                    AssassinAchievement(game);
+                    break;
+                case 3:
+                    ThievesAchievement(game);
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void GoldAchievement(IGame game)
+        {
+            if ((game as Game).gold >= 20000 && !_goldAchievement)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Achievement unlock: Reach 200 dollars. Price == 20 dollars");
+                Console.ResetColor();
+                _goldAchievement = true;
+            }
+        }
+
+        private void FoolsAchievement(IGame game)
+        {
+            this._countFool++;
+            switch (_countFool)
+            {
+                case 1:
+                    Message(1, "Fools", 5);
+                    (game as Game).gold += 500;
+                    break;
+                case 5:
+                    Message(5, "Fools", 10);
+                    (game as Game).gold += 1000;
+                    break;
+                case 10:
+                    Message(10, "Fools", 15);
+                    (game as Game).gold += 1500;
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void BeggaryAchievement(IGame game)
+        {
+            this._countBeggary++;
+            switch (_countBeggary)
+            {
+                case 1:
+                    Message(1, "Beggary", 5);
+                    (game as Game).gold += 500;
+                    break;
+                case 5:
+                    Message(5, "Beggary", 10);
+                    (game as Game).gold += 1000;
+                    break;
+                case 10:
+                    Message(10, "Beggary", 15);
+                    (game as Game).gold += 1500;
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void AssassinAchievement(IGame game)
+        {
+            this._countAssassin++;
+            switch (_countAssassin)
+            {
+                case 1:
+                    Message(1, "Assassin", 5);
+                    (game as Game).gold += 500;
+                    break;
+                case 5:
+                    Message(5, "Assassin", 10);
+                    (game as Game).gold += 1000;
+                    break;
+                case 10:
+                    Message(10, "Assassin", 15);
+                    (game as Game).gold += 1500;
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void ThievesAchievement(IGame game)
+        {
+            this._countThieves++;
+            switch (_countThieves)
+            {
+                case 1:
+                    Message(1, "Thieves", 5);
+                    (game as Game).gold += 500;
+                    break;
+                case 3:
+                    Message(3, "Thieves", 10);
+                    (game as Game).gold += 1000;
+                    break;
+                case 6:
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine($"Achievement unlock: Meet all Thieves. Price == 15 dollars");
+                    Console.ResetColor();
+                    (game as Game).gold += 1500;
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void Message(int n, string name, int price)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine($"Achievement unlock: Meet {n} {name}  Price == {price} dollars");
+            Console.ResetColor();
+        }
+    }
+}
--- Ankh-Morpork/Game.cs
+++ Ankh-Morpork/Game.cs
@@ -4,16 +4,28 @@ using System.Text;
 
 namespace Ankh_Morpork
 {
-    public class Game
+    //For achievements
+    public interface IGame
+    {
+        void Attach(IAchievements achievements);
+
+        void Notify();
+
+        void Notify(int type);
+    }
+
+    public class Game : IGame
     {
         private bool alive;
         private byte thiefs;
-        private int gold;
+        public int gold { get; set; }
 
         private Fools fools;
         private Beggary beggary;
         private Assassins assasins;
-        
+        //achievements
+        private List<IAchievements> _achievements;
+
         public Game()
         {
             this.alive = true;
@@ -22,16 +34,18 @@ namespace Ankh_Morpork
             this.fools = new Fools();
             this.beggary = new Beggary();
             this.assasins = new Assassins();
+            this._achievements = new List<IAchievements>();
         }
 
         public void Play()
         {
+            Attach(new Achievements());
+
             while (alive && gold > 0)
             {
-                Console.WriteLine($"You current balance {gold / 100} dollars {gold % 100} pence");
+                Balance();
                 var thief = thiefs > 0 ? 4 : 3;
                 var situation = GetRandomNumber(thief);
-                
                 switch (situation)
                 {
                     case 0:
@@ -52,6 +66,14 @@ namespace Ankh_Morpork
                 Console.WriteLine("You lose");
         }
 
+        private void Balance()
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine($"You current balance {gold / 100} dollars {gold % 100} pence");
+            Console.ResetColor();
+            Notify();
+        }
+
         private int GetRandomNumber(int n)
         {
             var num = new Random();
@@ -61,22 +83,29 @@ namespace Ankh_Morpork
 
         private void MeetFools()
         {
+            Console.ForegroundColor = ConsoleColor.Cyan;
             Console.WriteLine("You meet the guild of Fools and Joculators and College of Clowns");
+            Console.ResetColor();
             var price = fools.GetFoolPrice();
             Console.WriteLine($"Put:\n" +
                               $"1 for \"Play\"\n" +
-                              $"2 for\"Skip\"");
+                              $"2 for \"Skip\"");
             var answer = Console.ReadLine();
 
             if (String.Compare(answer, "1", StringComparison.OrdinalIgnoreCase) == 0)
+            {
                 gold = Plus(gold, price);
+                Notify(0);
+            }
             else
                 Console.WriteLine("You skip proposition");
         }
 
         private void MeetBeggary()
         {
+            Console.ForegroundColor = ConsoleColor.Green;
             Console.WriteLine("You meet Ankh-Morpork Beggars' Guild");
+            Console.ResetColor();
             var price = beggary.GetBeggaryPrice();
             Console.WriteLine($"Put:\n" +
                               $"1 for \"Give alms\"\n" +
@@ -84,7 +113,10 @@ namespace Ankh_Morpork
             var answer = Console.ReadLine();
 
             if (String.Compare(answer, "1", StringComparison.OrdinalIgnoreCase) == 0)
+            {
                 gold = Minus(gold, price);
+                Notify(1);
+            }
             else
             {
                 Console.WriteLine("You will be chased to death");
@@ -94,7 +126,9 @@ namespace Ankh_Morpork
 
         private void MeetAssassins()
         {
+            Console.ForegroundColor = ConsoleColor.DarkGray;
             Console.WriteLine("You meet Ankh-Morpork Assassins' Guild");
+            Console.ResetColor();
             Console.WriteLine($"Someone wants to kill you\n" +
                               $"Put:\n" +
                               $"1 for \"Create contract\"\n" +
@@ -107,7 +141,10 @@ namespace Ankh_Morpork
                 var revardStr = Console.ReadLine();
                 var revard = int.Parse(revardStr) * 100;
                 if (assasins.AssassinAcceptContract(revard))
+                {
                     gold = Minus(gold, revard);
+                    Notify(2);
+                }
                 else
                 {
                     Console.WriteLine("Any assassin does not take the contract\nYou will be murdered");
@@ -123,14 +160,19 @@ namespace Ankh_Morpork
 
         private void MeetThieves()
         {
+            Console.ForegroundColor = ConsoleColor.Blue;
             Console.WriteLine("You meet guild of Thieves, Cutpurses and Allied Trades");
+            Console.ResetColor();
             Console.WriteLine($"Put:\n" +
                               $"1 for \"Give money (10 dollars)\"\n" +
                               $"2 for \"Give up\"");
             var answer = Console.ReadLine();
 
             if (String.Compare(answer, "1", StringComparison.OrdinalIgnoreCase) == 0)
+            {
                 gold = Minus(gold, 1000);
+                Notify(3);
+            }
             else
             {
                 Console.WriteLine("Game over");
@@ -145,5 +187,27 @@ namespace Ankh_Morpork
 
         //Method Minus created with similarly reason which Plus
         public int Minus(int a, int b) => a - b;
+
+        //achievements
+        public void Attach(IAchievements achievements)
+        {
+            this._achievements.Add(achievements);
+        }
+
+        public void Notify()
+        {
+            foreach (var achievement in _achievements)
+            {
+                achievement.Update(this, -1);
+            }
+        }
+
+        public void Notify(int type)
+        {
+            foreach (var achievement in _achievements)
+            {
+                achievement.Update(this, type);
+            }
+        }
     }
 }
